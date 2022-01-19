37cff2ddbd4198b45a97900d1e2c203dc4ffd366
Code Sandbox <hello@codesandbox.io>
2022-01-19 00:52:25 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d8dafdf440e91e92cd90d557437850cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12618d3f430a9b0d79e4ba68e31850c",
+    "url": "https://ooni0921.github.io/csb-pcq5o/index.html"
+  },
+  {
+    "revision": "416eee634d6acc084e16",
+    "url": "https://ooni0921.github.io/csb-pcq5o/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "415c043af7895b406db0",
+    "url": "https://ooni0921.github.io/csb-pcq5o/static/css/main.db430fef.chunk.css"
+  },
+  {
+    "revision": "416eee634d6acc084e16",
+    "url": "https://ooni0921.github.io/csb-pcq5o/static/js/2.5d72e1da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://ooni0921.github.io/csb-pcq5o/static/js/2.5d72e1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "415c043af7895b406db0",
+    "url": "https://ooni0921.github.io/csb-pcq5o/static/js/main.33f179dc.chunk.js"
+  },
+  {
+    "revision": "2cc0af20b476adc3da48",
+    "url": "https://ooni0921.github.io/csb-pcq5o/static/js/runtime-main.639a420d.js"
+  }
+]);
